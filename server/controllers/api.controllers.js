80b51c2fb26d6b1015f779d5fd2c02732128f3ab
Gene Horecka <gene@genefever.com>
2021-11-23 01:27:01 -0800

    Return list of JSON objects from script.py

--- server/controllers/api.controllers.js
+++ server/controllers/api.controllers.js
@@ -8,14 +8,14 @@ const sayHello = (req, res) => {
   let options = {
     mode: 'json',
     scriptPath: path.join(__dirname, '..', 'python'),
-    args: ['Server is running'],
+    args: [req.query.query],
   }
 
   PythonShell.run('script.py', options, function (err, results) {
     if (err) throw err
 
-    // results is an array consisting of messages collected during execution
-    console.log('results: %j', results)
+    // 'results' is an array consisting of messages collected during execution.
+    // console.log('results: %j', results)
     // res.send(`<h2>${results[0]}</h2>`)
     res.send(results[0])
   })
