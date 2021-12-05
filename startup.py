48d68450b7cd1804075923472c8e8bbbda62a999
Gene Horecka <gene@genefever.com>
2021-12-04 23:34:51 -0800

    Add logs to startup.py

--- startup.py
+++ startup.py
@@ -63,6 +63,25 @@ def download_dir(prefix, local, bucket, client=s3_client):
 print("-----> Running startup.py...")
 print("-----> Downloading index files from AWS S3...")
 
+
+print("Path at terminal when executing this file")
+print(os.getcwd() + "\n")
+
+print("This file path, relative to os.getcwd()")
+print(__file__ + "\n")
+
+print("This file full path (following symlinks)")
+full_path = os.path.realpath(__file__)
+print(full_path + "\n")
+
+print("This file directory and name")
+path, filename = os.path.split(full_path)
+print(path + ' --> ' + filename + "\n")
+
+print("This file directory only")
+print(os.path.dirname(full_path))
+
+'''
 os.chdir('./python')
 
 # List of tuples containing files/folders to download from S3
@@ -71,5 +90,6 @@ files = [('final_compact_df.csv', '.'), ('idx/', '.'), ('wikipedia.dat', 'wikipe
 
 for file, dest in files:
     download_dir(file, dest, S3_BUCKET_NAME)
-    
+'''
+
 print("-----> Completed startup.py!")
