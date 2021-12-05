26f2678667943a3bf0a56c6c8888f6b654eccba9
Gene Horecka <gene@genefever.com>
2021-12-05 02:57:50 -0800

    Run app on localhost works

--- controllers/api.controllers.js
+++ controllers/api.controllers.js
@@ -4,12 +4,13 @@ const path = require('path')
 const getRankedWikiData = (req, res) => {
   let query_str = req.query.q
 
-  // TODO delete pythonPath on deployment.
+  // TODO remove pythonPath on deployment.
+  // TODO For testing locally, change the pythonPath to the path of your python 3.5.10 verion.
   let options = {
     mode: 'json',
     scriptPath: path.join(__dirname, '..', 'python'),
     args: ['config.toml', query_str],
-    // pythonPath: '/Users/genehorecka/.pyenv/versions/3.5.10/bin/python',
+    pythonPath: '/Users/genehorecka/.pyenv/versions/3.5.10/bin/python',
   }
 
   PythonShell.run('searchWiki.py', options, function (err, results) {
--- startup.py
+++
@@ -1,9 +0,0 @@
-import os
-import shutil
-
-print("-----> Running startup.py...")
-print("-----> Moving downloaded files from AWS S3...")
-shutil.move("/app/wikipedia.dat", "/app/python/wikipedia/wikipedia.dat")
-shutil.move("/app/final_compact_df.csv", "/app/python/final_compact_df.csv")
-print(os.getcwd())
-print("-----> Completed startup.py!")
